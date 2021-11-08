1803532a6d086a690fd0d2c3f36dd0d26d76f263
Devin Lu <ludevinl@sfu.ca>
2021-11-07 17:41:42 -0800

    modularized stuff

--- lichess.py
+++ lichess.py
@@ -1,16 +1,29 @@
 import berserk
 import pandas as pd
 
+# token expires in 30 days
 API_TOKEN = "lip_bYPixWzHsRrAjtPMeUwP"
+
+session = berserk.TokenSession(API_TOKEN)
+client = berserk.Client(session=session)
 
 def getLichessToken():
     return API_TOKEN
 
-def getLichessDataByPlayerAsJSON(client, username, max, color):
-    return client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max, color=color)
+def getColor(color):
+    if color.lower() == 'white':
+        return berserk.Color.WHITE
 
-def saveLichessDataByPlayerToCSV(client, username, max, color):
-    lst = list(client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max, color=color))
+    return berserk.Color.BLACK
+
+
+def getLichessDataByPlayerAsJSON(username, max, color):
+
+    return client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max, color=getColor(color))
+
+def saveLichessDataByPlayerToCSV(username, max, color):
+    
+    lst = list(client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max, color=getColor(color)))
     
     df = pd.DataFrame(lst)
     filename = username.lower()
--- ml.py
+++ ml.py
@@ -1,8 +1,23 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-def rfc(lst):
-    model = RandomForestClassifier()
+def rfc(file):
+    # Index(['id', 'rated', 'variant', 'speed', 'perf', 'createdAt', 'lastMoveAt',
+    #    'status', 'players', 'winner', 'opening', 'moves', 'clock',
+    #    'tournament'],
+    #   dtype='object')
+    df = pd.read_csv(file)
+    y = df['winner']
+    dropped = df.drop(['winner'], axis=1)
+    x = dropped.loc[:, 'rated':'tournament']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    # need to encode strings to numeric values before training
+    model = RandomForestClassifier().fit(x_train, y_train)
+
+    print(model.score(x_test, y_test))
     
 
 def main():

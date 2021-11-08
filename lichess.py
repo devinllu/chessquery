c77543c2c6778d7a868c7144ee81488b5c49caeb
Devin Lu <ludevinl@sfu.ca>
2021-11-07 16:45:34 -0800

    modularized, got some data

--- lichess.py
+++ lichess.py
@@ -6,9 +6,14 @@ API_TOKEN = "lip_bYPixWzHsRrAjtPMeUwP"
 def getLichessToken():
     return API_TOKEN
 
-def getLichessDataByPlayerAsJSON(client, username, max):
-    return client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max)
+def getLichessDataByPlayerAsJSON(client, username, max, color):
+    return client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max, color=color)
 
+def saveLichessDataByPlayerToCSV(client, username, max, color):
+    lst = list(client.games.export_by_player(username=username, as_pgn=False, rated=True, opening=True, max=max, color=color))
+    
+    df = pd.DataFrame(lst)
+    df.to_csv(f"{username}.csv", index=False)
 
 def main():
     pass
--- script.py
+++ script.py
@@ -1,19 +1,15 @@
 import lichess as li
-import pandas as pd
+
+
 import berserk
-import json
-from pprint import pprint
 
 def main():
     LICHESS_API_TOKEN = li.getLichessToken()
     session = berserk.TokenSession(LICHESS_API_TOKEN)
     client = berserk.Client(session=session)
 
-    lusthetics = li.getLichessDataByPlayerAsJSON(client, "Lusthetics", 10)
-    lusthetics = list(lusthetics)
-    # df = pd.read_json(lusthetics)
 
-    pprint(lusthetics[0])
+    li.saveLichessDataByPlayerToCSV(client=client, username="Lusthetics", max=1000, color=berserk.Color.WHITE)
 
 if __name__ == "__main__":
     main()

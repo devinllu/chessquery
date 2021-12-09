09c3a62202b2937b60b0e4e548ba267a41b09bd0
Devin <devinllu@hotmail.com>
2021-12-09 15:14:19 -0800

    computing feature importances

--- ml.py
+++ ml.py
@@ -1,23 +1,26 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
+import numpy as np
+import matplotlib.pyplot as plt
 
 def predict(file):
     # Index(['id', 'rated', 'variant', 'speed', 'perf', 'createdAt', 'lastMoveAt',
     #    'status', 'players', 'winner', 'opening', 'moves', 'clock',
     #    'tournament'],
     #   dtype='object')
+    # event,site,white,black,result,utcdate,utctime,whiteelo,blackelo,whiteratingdiff,blackratingdiff,eco,opening,timecontrol,termination,moves_played,played_f3,castled,rating_difference,rating_average
     df = pd.read_csv(file)
     df = df[df['event'] == 2]
 
     # dropped = df.drop(['winner'], axis=1)
     # x = dropped.loc[:, 'rated':'tournament']
-    x = df[['blackelo', 'whiteelo', 'eco']].copy()
+    x = df.drop('')
 
     
     label_encoder = LabelEncoder()
@@ -48,10 +51,56 @@ def predict(file):
 
     for i, j in zip(lst, names):
         print(f"{j}: {i}")
-    
+
+def measure_attributes(file):
+    df = pd.read_csv(file)
+    df = df.drop('site', axis=1)
+
+    print(df[:3])
+
+    # df.dropna(inplace=True)
+
+    print(f"shape: {df.shape}")
+
+    x = df.drop('result', axis=1)
+
+    ohe = OneHotEncoder()
+    lbe = LabelEncoder()
+    y_encoder = LabelBinarizer()
+
+    for col in x.columns:
+        x[col] = lbe.fit_transform(x[col])
+    x = ohe.fit_transform(x)
+
+    y = lbe.fit_transform(df['result'])
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    model = RandomForestClassifier().fit(x_train, y_train)
+    print(f"Gradient Boosting Score: {model.score(x_test, y_test)}")
+    print(f"Feature Importances: {model.feature_importances_}")
+
+    features = ['event',"white","black","utcdate","utctime","whiteelo","blackelo","whiteratingdiff","blackratingdiff","eco","opening","timecontrol","termination","moves_played","played_f3","castled","rating_difference","rating_average"]
+    importances = model.feature_importances_
+    indices = np.argsort(importances)
+
+    print(indices)
+    print(len(features))
+    print(importances)
+    print(len(importances))
+    print(len(indices))
+
+    plt.title('Feature Importances')
+    plt.plot(importances)
+    # plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+    # plt.yticks(range(len(indices)), [features[i] for i in indices])
+    # plt.xlabel('Relative Importance')
+    plt.savefig('graph.png')
+    plt.show()
+
 
 def main():
-    pass
+    measure_attributes("data/lichess-std-big.csv")
 
 if __name__ == "__main__":
     main()
